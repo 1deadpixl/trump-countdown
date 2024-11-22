dc2e3c746d547ec298f24938a1faba3421f5eb79
Philippe Enzler <phil@enzler.dev>
2024-11-21 17:40:56 -0800

    Add word switcher

---
+++ src/components/WordSwitcher.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { onMounted } from "vue";
+import wordSwitcher from "word-switcher";
+const words = ["America's Freedom", "Better Choices", "Strong Economy"];
+
+onMounted(() => {
+  const target = document.querySelector("#word-switch");
+  wordSwitcher(target, words, { random: true, animationDuration: 250 });
+});
+</script>
+
+<template>
+  <div class="text-center leading-tight my-20">
+    <p class="text-[72px] font-bold">
+      Counting down to<br /><span id="word-switch"></span>
+    </p>
+  </div>
+</template>
+
+<style lang="css">
+.word-switcher-enter-active,
+.word-switcher-leave-active {
+  transition: opacity 1s;
+}
+.word-switcher-enter,
+.word-switcher-leave-to {
+  opacity: 0;
+}
+</style>
